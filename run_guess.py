7c87c1d175381947350411dc7f8fd5cbd65870fb
Arina <Arina.Kharlamova@mbzuai.ac.ae>
2025-04-07 00:18:29 +0300

    wip

--- run_guess.py
+++ run_guess.py
@@ -3,12 +3,15 @@
 
 from src.guess.guess import ConfigType, Guess
 from src.domain.datasets.UnixCommandDataset import UnixCommandDataset  # Register dataset
-from src.domain.datasets.ProjectEulerDataset import ProjectEulerDataset  # Register dataset
+from src.domain.datasets.ProjectEulerDataset import ProjectEulerDataset
+from src.sketch.sketch import Sketch  # Register dataset
 
 if __name__ == "__main__":
-    guess = Guess(ConfigType.QWEN_X862ARM64)  # Using QWEN model for ARM to x86 translation
+    # Using QWEN model for ARM to x86 translation
+    guess = Guess(ConfigType.QWEN_X862ARM64)
     predictions = guess.guess()
     print("Inference complete.")
     metrics = guess.evaluate(predictions)
     print("Evaluation complete.")
     print(metrics)
+    sketch = Sketch()
