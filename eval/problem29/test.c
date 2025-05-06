11e21e80a18ecdde09ebf1fcb7b6dc7910f4e036
Alex Pretko <adpretko@gmail.com>
2025-05-06 09:56:50 +0000

    Added eval files for testing

---
+++ eval/problem29/test.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+char* func0(char** strings, int count);
+
+int main() {
+    char* result;
+    char* empty_strings[] = {};
+    result = func0(empty_strings, 0);
+    assert(strcmp(result, "") == 0);
+    free(result);
+
+    char* strings1[] = {"x", "y", "z"};
+    result = func0(strings1, 3);
+    assert(strcmp(result, "xyz") == 0);
+    free(result);
+
+    char* strings2[] = {"x", "y", "z", "w", "k"};
+    result = func0(strings2, 5);
+    assert(strcmp(result, "xyzwk") == 0);
+    free(result);
+
+    return 0;
+}
---
+++ eval/problem78/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+int func0(int a);
+
+int main() {
+    assert(func0(1) == 1);
+    assert(func0(2) == 0);
+    assert(func0(-1) == 1);
+    assert(func0(64) == 1);
+    assert(func0(180) == 0);
+    assert(func0(1000) == 1);
+    assert(func0(0) == 1);
+    assert(func0(1729) == 0);
+
+    printf("All tests passed.\n");
+    return 0;
+}
